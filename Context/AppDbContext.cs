734e9a50e9bfa72b5017e84abaada52868f3badd
diogoeugenio920@gmail.com <diogoeugenio222@gmail.com>
2026-05-07 20:44:10 -0300

    chore initial backend setup with ef core

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BarberFlow.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberFlow.API.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
