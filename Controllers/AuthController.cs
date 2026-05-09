1ef1a9e51bbfd84d612408713fb664c88f75eaa7
diogoeugenio920@gmail.com <diogoeugenio222@gmail.com>
2026-05-09 00:24:12 -0300

    feat: implement authentication endpoints

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,86 @@
+﻿using BarberFlow.API.Context;
+using BarberFlow.API.DTOs;
+using BarberFlow.API.Entities;
+using BarberFlow.API.Services.Auth;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberFlow.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    private readonly JwtService _jwtService;
+
+    public AuthController(
+        AppDbContext context,
+        JwtService jwtService)
+    {
+        _context = context;
+        _jwtService = jwtService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<AuthResponseDto>> Register(
+        RegisterRequestDto request)
+    {
+        var userExists = await _context.Users
+            .AnyAsync(x => x.Email == request.Email);
+
+        if (userExists)
+        {
+            return BadRequest("Email already exists.");
+        }
+
+        var user = new User
+        {
+            Name = request.Name,
+            Email = request.Email,
+            Phone = request.Phone,
+
+            // TEMPORÁRIO
+            PasswordHash = request.Password
+        };
+
+        _context.Users.Add(user);
+
+        await _context.SaveChangesAsync();
+
+        var token = _jwtService.GenerateToken(user);
+
+        return Ok(new AuthResponseDto
+        {
+            Token = token
+        });
+    }
+    [HttpPost("login")]
+    public async Task<ActionResult<AuthResponseDto>> Login(
+    LoginRequestDto request)
+    {
+        var user = await _context.Users
+            .FirstOrDefaultAsync(x => x.Email == request.Email);
+
+        if (user == null)
+        {
+            return Unauthorized("Invalid email or password.");
+        }
+
+        // TEMPORÁRIO
+        if (user.PasswordHash != request.Password)
+        {
+            return Unauthorized("Invalid email or password.");
+        }
+
+        var token = _jwtService.GenerateToken(user);
+
+        return Ok(new AuthResponseDto
+        {
+            Token = token
+        });
+    }
+
+
+}
